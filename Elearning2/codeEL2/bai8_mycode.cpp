226cb321b9d2f9e1e1e1984c86fc9c209925622d
Nguyen Le Huy Tam <huytamnguyenle2608@gmail.com>
2026-04-03 02:34:42 +0700

    Update bai8_mycode.cpp
    No divide and conquer

--- Elearning2/codeEL2/bai8_mycode.cpp
+++ Elearning2/codeEL2/bai8_mycode.cpp
@@ -4,6 +4,19 @@
 using namespace std;
 
 int main(){
-  
-  return 0;
+    int n;
+        cout << "Nhap n: ";
+        cin >> n;
+        vector<int> a(n);
+        for(int &x : a) cin >> x;
+
+        int k;
+        cout << "Nhap k: ";
+        cin >> k;
+
+        sort(a.begin(), a.end());
+        cout << "So lon thu " << k << " trong mang (co so trung) la: " << a[n-k] << endl;
+        set<int> noDupe(a.begin(), a.end());
+        cout << "So lon thu " << k << " trong mang (khong co so trung) la: " << a[noDupe.size() - k] << endl;
+    return 0;
 }
