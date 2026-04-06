0662a746274021094300d6383c763a9bef652090
Nguyen Le Huy Tam <huytamnguyenle2608@gmail.com>
2026-04-07 01:36:14 +0700

    Update server.js
    Up toàn bộ file server

--- Đồ_án_cuối_kì/server/server.js
+++ Đồ_án_cuối_kì/server/server.js
@@ -1,1 +1,37 @@
+const express = require("express");
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
 
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server OK");
+});
+
+app.post("/solve", (req, res) => {
+    const customers = req.body.customers;
+
+    let input = customers.length + "\n";
+    customers.forEach(c => {
+        input += `${c.x} ${c.y}\n`;
+    });
+
+    const inputPath = path.join(__dirname, "../data/input.txt");
+    fs.writeFileSync(inputPath, input);
+
+    const exePath = path.join(__dirname, "../backend-cpp/vrp.exe");
+
+    exec(`${exePath} < "${inputPath}"`, (err, stdout, stderr) => {
+        if (err) {
+            console.error(stderr);
+            return res.status(500).send("Lỗi chạy C++");
+        }
+        res.send(stdout);
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server chạy tại http://localhost:3000");
+});
